5c512f9aabeeff777a3935f272f1f73f1549b91a
Swaroop Ankam <swaroopankam04@gmail.com>
2023-04-24 01:19:08 +0530

    Added other files

---
+++ clint/search.js
@@ -0,0 +1,14 @@
+function search_food() {
+	let input = document.getElementById('searchbar').value
+	input=input.toLowerCase();
+	let x = document.getElementsByClassName('food');
+	
+	for (i = 0; i < x.length; i++) {
+		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+			x[i].style.display="none";
+		}
+		else {
+			x[i].style.display="list-item";				
+		}
+	}
+}
